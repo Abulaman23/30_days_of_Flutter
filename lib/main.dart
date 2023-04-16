4d453b5a9d3c742d9afe9d011311c53dfb7d1fdb
Abulaman23 <abul2002aman@gmail.com>
2023-04-16 20:54:21 +0530

    DAY 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalogues/home_page.dart';
+import 'package:flutter_catalogues/pages/home_page.dart';
+import 'package:flutter_catalogues/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +12,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Homepage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.amber),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => Homepage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,16 +2,15 @@ import 'package:flutter/material.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Catalog App"),
+        title: Text("Catalogue App"),
       ),
       body: Center(
         child: Container(
-          child: Text("Welcome to 30 days of flutter"),
+          child: Text("Welcome to 30 days of Flutter"),
         ),
       ),
       drawer: Drawer(),
